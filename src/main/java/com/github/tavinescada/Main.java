a9eca68ac046b5149c84a1742a93d1090a9bbb81
Otávio Francisco Sabino Tavares <114409083+tavinEscada@users.noreply.github.com>
2025-07-18 16:16:00 -0300

    Add files via upload

---
+++ src/main/java/com/github/tavinescada/Main.java
@@ -0,0 +1,9 @@
+package com.github.tavinescada;
+
+public class Main {
+    public static void main(String[] args) {
+        Funcs funcoes = new Funcs();
+
+        funcoes.retornaData();
+    }
+}
